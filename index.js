b331c8d3321590aac13e828fe3df3a378e14e1b8
gabrielalegrete <gabrielalegrete1@gmail.com>
2021-07-01 04:23:23 -0300

    finish connection

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 require('dotenv').config()
 const express = require('express')
-global.db = require('./src/infrastructure/config/database')
-
+const app = express()
+const router = express.Router()
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const cors = require('cors')
+var corsOptions = {
+  origin: "http://localhost:8081"
+}
+global.db = require('./src/infrastructure/config/database')
 
-const app = express();
+app.use(cors(corsOptions));
 
 app.get('/', async (req, res) => {
     res.send('Home');
@@ -14,12 +19,10 @@ app.get('/', async (req, res) => {
 app.get('/clients', async (req, res, next) => {
   try {
     const docs = await global.db.findAll()
-    res.json({title: 'Lista de Clientes', docs });
+    res.json(docs);
   } catch (err) {
     next(err)
   }
 })
-
-
 
 app.listen(PORT, HOST)
