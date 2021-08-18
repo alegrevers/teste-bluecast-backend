0c8dbc01b7e471123bddfda852e94bf6aa91d246
gabrielalegrete <56418306+gabrielalegrete@users.noreply.github.com>
2021-08-18 12:19:11 -0300

    Update database.js

--- src/infrastructure/config/database.js
+++ src/infrastructure/config/database.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.${process.env.DB_CLUSTER_CODE}.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`
 
 MongoClient.connect(url, { useNewUrlParser: true })
-  .then(conn => global.conn = conn.db("triyo-test"))
+  .then(conn => global.conn = conn.db("bluecast-test"))
   .catch(err => console.log(err))
 
 function findAll() {
